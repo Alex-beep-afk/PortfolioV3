5dc7c7d9b5bd37219a2fd805f0eae5dcc62c28b1
Alexandre_maison <alexandre.prigent2910@gmail.com>
2025-11-27 17:07:37 +0100

    chore(cleanup, security, templates): remove unused hello controller, update security paths, and enhance base template structure

---
+++ src/Controller/BackOffice/ExperiencesController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Controller\BackOffice;
+
+
+use App\Entity\Experience;
+use App\Form\ExperienceFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+final class ExperiencesController extends AbstractController
+{
+    
+    public function __construct(
+    private readonly EntityManagerInterface $em
+    ){}
+
+    #[Route('/user/experiences', name: 'experiences.index')]
+    public function index(): Response
+    {
+        $user = $this->getUser();
+        $experiences = $this->em->getRepository(Experience::class)->findBy(['user' => $user]);
+
+        return $this->render('backOffice/Experience/index.html.twig', [
+            'experiences' => $experiences,
+
+        ]);
+    }
+    #[Route('/user/experiences/new', name: 'experiences.new')]
+    public function new(Request $request): Response
+    {
+        $user = $this->getUser();
+        $experience = new Experience();
+        $experience->setUser($user);
+
+        $form = $this->createForm(ExperienceFormType::class, $experience);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->em->persist($experience);
+            $this->em->flush();
+            $this->addFlash('success', 'Experience créée avec succès');
+            return $this->redirectToRoute('experiences.index');
+        }
+
+        return $this->render('backOffice/Experience/new.html.twig', [
+            'form' => $form
+        ]);
+    }
+
+    #[Route('/user/experiences/{id}/edit', name: 'experiences.edit')]
+    public function edit(Request $request, Experience $experience): Response
+    {
+        $user = $this->getUser();
+        if ($user !== $experience->getUser()) {
+            $this->addFlash('error', 'Cette ressource ne vous appartient pas');
+            return $this->redirectToRoute('experiences.index');
+        }
+
+        $form = $this->createForm(ExperienceFormType::class, $experience);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->em->flush();
+            $this->addFlash('success', 'Experience modifiée avec succès');
+            return $this->redirectToRoute('experiences.index');
+        }
+        return $this->render('backOffice/Experience/edit.html.twig', [
+            'form' => $form
+        ]);
+    }
+    #[Route('/user/experiences/{id}/delete', name: 'experiences.delete')]
+    public function delete(Request $request, Experience $experience): Response
+    {
+        $user = $this->getUser();
+        if ($user !== $experience->getUser()) {
+            $this->addFlash('error', 'Cette ressource ne vous appartient pas');
+            return $this->redirectToRoute('experiences.index');
+        }
+
+        if ($this->isCsrfTokenValid('delete' . $experience->getId(), $request->request->get('_token'))) {
+            $this->em->remove($experience);
+            $this->em->flush();
+            $this->addFlash('success', 'Experience supprimée avec succès');
+        } else {
+            $this->addFlash('error', 'Token CSRF invalide');
+        }
+            return $this->redirectToRoute('experiences.index');
+    }
+}
---
+++ src/Controller/BackOffice/UserDashboardController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller\BackOffice;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class UserDashboardController extends AbstractController
+{
+    #[Route('/user/dashboard', name: 'user.dashboard')]
+    public function index(): Response
+    {
+        return $this->render('backOffice/userDashboard.html.twig', [
+        ]);
+    }
+}
